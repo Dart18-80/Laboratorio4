08aa69dfeb553d0cad99b31ff1dcf3275df417df
Diego Ruiz <diegoruiztrabanino13@gmail.com>
2021-10-05 01:19:33 -0600

    Cifrado Completo y descifrado

--- LibreriaDeCifrado/CifradoDeCesar.cs
+++ LibreriaDeCifrado/CifradoDeCesar.cs
@@ -13,7 +13,7 @@ namespace LibreriaDeCifrado
 
         public void CreateDiccionary() 
         {
-            for (byte i = 32; i <= 126; i++) 
+            for (byte i = 33; i <= 126; i++) 
             {
                 InitialDiccionary.Add(i, (char)i);
             }
@@ -24,17 +24,17 @@ namespace LibreriaDeCifrado
             byte[] Composition = Encoding.ASCII.GetBytes(key);
             byte[] NewKey = Composition.Distinct().ToArray();
             int Long = NewKey.Length;
-            byte FirstValue = 32;
+            byte FirstValue = 33;
             for (int i = Long; i >= 0; i--) 
             {
-                InitialDiccionary[NewKey[i]] = "¬";
+                InitialDiccionary[NewKey[i]] = '¬';
             }
             for (int i = 0; i >= Long; i++) 
             {
                 NewDiccionary.Add(FirstValue,(char)NewKey[i]);
                 FirstValue++;
             }
-            for (byte i = 32; i<=126; i++) 
+            for (byte i = 33; i<=126; i++) 
             {
                 char Aux = (char)InitialDiccionary[i];
                 if (Aux != '¬') 
@@ -45,9 +45,38 @@ namespace LibreriaDeCifrado
             }
         }
 
-        public void Encrypt(byte[] Cadena,string Key) 
+        public string Encrypt(object Cadena, string Key)
         {
+            CreateDiccionary();
+            string Archivo = Convert.ToString(Cadena);
+            byte[] Traducir = Encoding.ASCII.GetBytes(Archivo);
+            int LongitudArchivo = Traducir.Length;
+            CreateNewDiccionary(Key);
+            string NewArchivo = "";
+            for (int i = 0; i <= LongitudArchivo; i++) 
+            {
+                NewArchivo += NewDiccionary[Traducir[i]];
+            }
+            NewDiccionary.Clear();
+            InitialDiccionary.Clear();
+            return NewArchivo;
+        }
 
+        public string Decrypt(object Cadena, string Key) 
+        {
+            CreateDiccionary();
+            string Archivo = Convert.ToString(Cadena);
+            byte[] Traducir = Encoding.ASCII.GetBytes(Archivo);
+            int LongitudArchivo = Traducir.Length;
+            CreateNewDiccionary(Key);
+            string NewArchivo = "";
+            for (int i = 0; i <= LongitudArchivo; i++)
+            {
+                NewArchivo += NewDiccionary[Traducir[i]];
+            }
+            NewDiccionary.Clear();
+            InitialDiccionary.Clear();
+            return NewArchivo;
         }
     }
 }
