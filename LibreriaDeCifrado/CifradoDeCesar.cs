71e40ef50d1049840999c74e7d239581637f397c
Diego Ruiz <diegoruiztrabanino13@gmail.com>
2021-10-05 16:19:41 -0600

    Mas caracteres

--- LibreriaDeCifrado/CifradoDeCesar.cs
+++ LibreriaDeCifrado/CifradoDeCesar.cs
@@ -13,7 +13,7 @@ namespace LibreriaDeCifrado
 
         public void CreateDiccionary() 
         {
-            for (byte i = 33; i <= 126; i++) 
+            for (byte i = 32; i <= 126; i++) 
             {
                 InitialDiccionary.Add(i, (char)i);
             }
@@ -24,7 +24,7 @@ namespace LibreriaDeCifrado
             byte[] Composition = Encoding.ASCII.GetBytes(key);
             byte[] NewKey = Composition.Distinct().ToArray();
             int Long = NewKey.Length;
-            byte FirstValue = 33;
+            byte FirstValue = 32;
             for (int i = Long; i >= 0; i--) 
             {
                 InitialDiccionary.Remove(NewKey[i]);
@@ -35,7 +35,7 @@ namespace LibreriaDeCifrado
                 NewDiccionary.Add(FirstValue,(char)NewKey[i]);
                 FirstValue++;
             }
-            for (byte i = 33; i<=126; i++) 
+            for (byte i = 32; i<=126; i++) 
             {
                 char Aux = (char)InitialDiccionary[i];
                 if (Aux != '¬') 
